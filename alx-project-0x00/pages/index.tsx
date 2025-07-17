22b9d83bc0d29e18062b628fce722b117ede8448
MercyXp <munzenzimercy2024@gmail.com>
2025-07-17 13:16:49 +0200

    changed function to an arrow function

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,7 +1,10 @@
-export default function Home() {
+const Home: React.FC = () => {
   return (
-    <main>
-      <h1>Welcome to the Home Page</h1>
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>Airbnb Application Clone system</h1>
+      <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
     </main>
   )
 }
+
+export default Home;

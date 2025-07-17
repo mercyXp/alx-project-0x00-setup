558810e7a7e84dbeb4fa4dddf46b2434c8d56683
MercyXp <munzenzimercy2024@gmail.com>
2025-07-17 15:26:33 +0200

    button component added

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,11 +1,28 @@
-import Card from "@/components/Card"
+import Card from "@/components/Card";
+import Button from "@/components/button";
 
-const Landing: React.FC =  () => {
-  return (
-    <div>
-      <h1 className=" text-xl font-extralight">Landing Page</h1>
-      <Card />
-    </div>
-  )
+
+const sizes = {
+    small: "text-sm",
+    medium: "text-base",
+    large: "text-lg"
 }
-export default Landing
+
+const shapes = [ "rounded-sm", "rounded-md", "rounded-lg", "rounded-full" ]
+
+const Landing : React.FC = () => {
+
+    return (
+        <div>
+            <h1 className="text-xl font-extralight">Landing Page</h1>
+            <Card />
+            <div className="flex flex-col items-center mt-4">
+                <Button title="Click Me" styles={`${sizes.small} ${shapes[0]} bg-blue-500 text-white`} />
+                <Button title="Click Me" styles={`${sizes.medium} ${shapes[1]} bg-green-500 text-white mt-2`} />
+                <Button title="Click Me" styles={`${sizes.large} ${shapes[2]} bg-red-500 text-white mt-2`} />
+            </div>
+        </div>
+    )
+}
+
+export default Landing;

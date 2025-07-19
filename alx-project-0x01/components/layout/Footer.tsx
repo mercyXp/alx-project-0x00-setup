b17930f71ffd0590127d1ff69c5e135aa81f3ae6
Mercy <165831481+mercyXp@users.noreply.github.com>
2025-07-19 08:13:27 +0200

    Delete alx-project-0x01 directory

--- alx-project-0x01/components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer: React.FC = () => {
-  return (
-    <footer className="bg-gray-800 text-white text-center py-4 mt-auto">
-      <p>&copy; {new Date().getFullYear()} Daily Contents. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
